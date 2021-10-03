a0136ee6cbbd9d28279cc1532f8de18d57ad41a6
Michelle-Marie Schiller <michellemarie.schiller@outlook.com>
2021-10-03 08:45:21 +0200

    Added main memory initialization code
    Added: [ARM9] Main memory initialization code

--- source/arm9/boot.asm
+++ source/arm9/boot.asm
@@ -1,10 +1,11 @@
-#########################################################################################
+############################################################################################################################
 # replacemenDS/ARM9 - Open source ARM9 boot ROM replacement.
 # Copyright (C) 2021  Michelle-Marie Schiller
-#########################################################################################
+############################################################################################################################
 # boot.asm - ARM9 exception vectors and boot code.
-#########################################################################################
+############################################################################################################################
 
+# Includes
 .include "inc/memory.asm"
 .include "inc/mmio.asm"
 .include "inc/psr.asm"
@@ -15,28 +16,30 @@ ExceptionVectors:
     b ResetHandler
 
     # Normally, this vector would branch to an Undefined Instruction exception handler.
-    # However, we won't use this type of exception. 
+    # However, we won't use this type of exception.
     b UnhandledException
 
     # Branch to Supervisor code (SVC mode)
-    b SVCHandler
+    # b SVCHandler
+    b UnhandledException
 
     # Normally, this vector would branch to a Prefetch Abort exception handler.
-    # However, we won't use type of exception. 
+    # However, we won't use type of exception.
     b UnhandledException
 
     # Normally, this vector would branch to a Data Abort exception handler.
-    # However, we won't use type of exception. 
+    # However, we won't use type of exception.
     b UnhandledException
 
     # This type of exception is not supported on ARMv4T and higher.
     b UnhandledException
 
     # Branch to Interrupt code (IRQ mode)
-    b IRQHandler
+    # b IRQHandler
+    b UnhandledException
 
     # Normally, this vector would branch to a Fast Interrupt exception handler.
-    # However, we won't use type of exception. 
+    # However, we won't use type of exception.
     b UnhandledException
 
 UnhandledException:
@@ -54,7 +57,32 @@ ResetHandler:
 
     # This is a cold boot, perform hardware initialization
 
+    InitializeMainMemory:
+        ldr r1, .0
+        ldr r2, .0 + 4
+
+        # To initialize main memory, we have to first read the contents of its control
+        # register...
+        ldrh r0, [r1]
+
+        # , write back the data we've read twice...
+        strh r0, [r1]
+        strh r0, [r1]
+
+        # and write two arbitrary halfwords (can be the same data).
+        strh r0, [r1]
+        strh r0, [r1]
+
+        # The next read from a main memory address will set the new configuration data.
+        # Bits 1-21 of the address contain the new configuration data!
+        ldrh r0, [r2]
+
     sub pc, pc, 8
+
+    # Pool
+    .0:
+        .4byte ADDR_CR                                     ; [0x00] Main memory control register
+        .4byte ADDR_MainMemory + 0x400000 + (DATA_CR << 1) ; [0x04] New main memory configuration data
 
 WarmBoot:
     b WarmBoot
--- source/arm9/inc/mmio.asm
+++ source/arm9/inc/mmio.asm
@@ -1,14 +1,28 @@
-#########################################################################################
+############################################################################################################################
 # replacemenDS/ARM9 - Open source ARM9 boot ROM replacement.
 # Copyright (C) 2021  Michelle-Marie Schiller
-#########################################################################################
+############################################################################################################################
 # mmio.asm - ARM9 Memory-mapped IO definitions.
-#########################################################################################
+############################################################################################################################
 
-# NOTE: All registers are specified as offsets to the MMIO base address.
+# NOTE: All registers (except CR) are specified as offsets to the MMIO base address.
 
 # System Control registers
 .set IO_POSTFLG, 0x300
 
 # IO_POSTFLG
 .set POSTFLG_BootCompleted, 1
+
+# Main Memory Control register
+.set ADDR_CR, 0x027FFFFE
+
+# CR
+.set CR_WELevelControl, 1 <<  7 ; WE Level Control
+.set CR_RisingEdge    , 1 <<  9 ; Rising edge is valid
+.set CR_Sequential    , 1 << 11 ; Sequential burst
+.set CR_4Cycles       , 2 << 12 ; Read latency is 4 cycles
+.set CR_Continuous    , 7 << 16 ; Continuous burst
+.set CR_NoPartial     , 3 << 19
+
+# Default CR data
+.set DATA_CR, CR_NoPartial | CR_Continuous | CR_4Cycles | CR_Sequential | CR_RisingEdge | 0x100 | CR_WELevelControl | 0x7F
